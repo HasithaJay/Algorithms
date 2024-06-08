789df4016ca0e35d19604171b3a17f0d641ac361
Hasitha Jayasinghe <hasithasrj@gmail.com>
2024-06-09 00:15:02 +0530

    Binary search implementation

---
+++ Algorithms/BinarySearchClass.cs
@@ -0,0 +1,44 @@
+﻿namespace Algorithms
+{
+    internal class BinarySearchClass
+    {
+        internal static int BinarySearch(int[] integerArr, int target)
+        {
+            int rangeStartingPoint = 0;
+            int rangeEndingPoint = integerArr.Length - 1;
+            
+
+            while(rangeStartingPoint <= rangeEndingPoint)
+            {
+                int midpoint = (rangeStartingPoint + rangeEndingPoint) / 2;
+
+                if (integerArr[midpoint] == target)
+                {
+                    return midpoint;
+                }
+                else if (integerArr[midpoint] > target)
+                {
+                    rangeEndingPoint = midpoint - 1;
+                }
+                else if (integerArr[midpoint] < target)
+                {
+                    rangeStartingPoint = midpoint + 1;
+                }
+            }
+
+            return -1;
+        }
+
+        internal static void Verify(int index)
+        {
+            if (index == -1)
+            {
+                Console.WriteLine("Item not found in the array!");
+            }
+            else
+            {
+                Console.WriteLine("Item found at index:" + index);
+            }
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -7,7 +7,9 @@
 
             int[] integerArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            LinearSearchClass.Verify(LinearSearchClass.LinearSearch(integerArr, 11));
+            //LinearSearchClass.Verify(LinearSearchClass.LinearSearch(integerArr, 11));
+
+            BinarySearchClass.Verify(BinarySearchClass.BinarySearch(integerArr, 1));
         }
     }
 }
