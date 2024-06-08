d198984dbddbdf94b502e0a70e47c9001519b238
Hasitha Jayasinghe <Hasitha Jayasinghe@DESKTOP>
2024-06-08 12:39:22 +0530

    Add project files.

---
+++ Algorithms/LinearSearchClass.cs
@@ -0,0 +1,14 @@
+﻿namespace Algorithms
+{
+    internal class LinearSearchClass
+    {
+        static int LinearSearch(int[] integerArr, int target)
+        {
+            for (int i = 0; i < integerArr.Length; i++)
+            {
+                if (integerArr[i] == target) return i;
+            }
+            return -1; 
+        }
+    }
+}
