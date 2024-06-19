091607e50fa1c63d9eb09d9d9be856ff6f3f4b0d
Hasitha Jayasinghe <hasithasrj@gmail.com>
2024-06-20 01:14:02 +0530

    Merge Sort Algorithm

---
+++ Algorithms/MergeSort.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+
+namespace Algorithms
+{
+    public static class MergeSortClass
+    {
+        //1.Devide
+        //2.Conquer
+        //3.Merge
+
+        public static List<int> MergeSort(List<int> theList)
+        {
+            //need to have a base case since we are dealing this recursively
+            if (theList.Count <= 1)
+            {
+                return theList;
+            }
+
+            int midPoint = theList.Count / 2;
+
+            List<int> left = new List<int>(theList[..midPoint]);
+            List<int> right = new List<int>(theList[(midPoint)..]);
+
+            List<int> sortedLeft = MergeSort(left);
+            List<int> sortedRight = MergeSort(right);
+
+            return Merge(sortedLeft, sortedRight);
+        }
+
+        private static List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> sortedList = new List<int>();
+            int i = 0, j = 0;
+
+            while (i < left.Count && j < right.Count)
+            {
+                if (left[i] < right[j])
+                {
+                    sortedList.Add(left[i]);
+                    i++;
+                }
+                else
+                {
+                    sortedList.Add(right[j]);
+                    j++;
+                }
+            }
+
+            while (i < left.Count)
+            {
+                sortedList.Add(left[i]);
+                i++;
+            }
+
+            while (j < right.Count)
+            {
+                sortedList.Add(right[j]);
+                j++;
+            }
+
+            return sortedList;
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -4,14 +4,25 @@
     {
         static void Main(string[] args)
         {
-
-            int[] integerArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            #region Basic Search Algorithms
+            //int[] integerArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             //LinearSearchClass.Verify(LinearSearchClass.LinearSearch(integerArr, 11));
 
             //BinarySearchClass.Verify(BinarySearchClass.BinarySearch(integerArr, 1));
 
-            RecursiveBinarySearchClass.Verify(BinarySearchClass.BinarySearch(integerArr, 10));
+            //RecursiveBinarySearchClass.Verify(BinarySearchClass.BinarySearch(integerArr, 10));
+            #endregion
+
+            #region Merge Sort
+            List<int> list = new List<int>{5, 4, 43, 21, 46, 75, 50, 100, 101};
+            var result = MergeSortClass.MergeSort(list);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
         }
     }
 }
